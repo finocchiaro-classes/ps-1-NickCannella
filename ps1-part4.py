a887e0d2f1b5f4625a950336ab5089519af2e4db
NickCannella <nickcannella19@gmail.com>
2025-09-06 15:19:09 -0400

    Update ps1-part4.py

--- ps1-part4.py
+++ ps1-part4.py
@@ -1,1 +1,18 @@
+score = 0
+priorarrests = int(input("Prior arrests: "))
+reason = input("Prior arrests for local ordinance (Y/N): ")
+age = int(input("Age at release: "))
+
+
+if priorarrests >= 2:
+    score = score+1
+if priorarrests >= 5:
+    score = score+1
+if reason == ("Y"):
+    score = score+1
+if 18 <= age <= 24:
+    score = score+1
+if age >= 40:
+    score = score-1
+print(f"The recidivism risk score is {score}")
 
